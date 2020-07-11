4b0a79015d193bb0c0036c2e195a990acdbbcf25
HassenH1 <hasansaid51@gmail.com>
2020-07-10 17:40:28 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+}); // I listen on the connection event for incoming sockets and log it to the console.
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(5000, () => {
+  console.log("listening on *:5000");
+});
