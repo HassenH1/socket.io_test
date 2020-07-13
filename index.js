ac97992e4f42cf242b15319f760defc8ad47d9ed
HassenH1 <hasansaid51@gmail.com>
2020-07-12 21:29:01 -0700

    PORT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 var io = require("socket.io")(http);
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -23,6 +24,6 @@ io.on("connection", (socket) => {
   });
 }); // I listen on the connection event for incoming sockets and log it to the console.
 
-http.listen(8080, "0.0.0.0", () => {
+http.listen(PORT, "0.0.0.0", () => {
   console.log("listening on *:5000");
 });
