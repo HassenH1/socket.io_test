f03a51095fb23225ae615f8a6529df1d37b4fb8a
HassenH1 <hasansaid51@gmail.com>
2020-07-13 23:50:58 -0700

    its not updating for some reason

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ const app = require("express")();
 const http = require("http").createServer(app);
 var io = require("socket.io")(http);
 const PORT = process.env.PORT || 8080;
-const users = [];
+let users = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
-  io.emit("connected", "A user has Connected");
+  io.emit("connected", "a user has connected");
 
   socket.on("new user", (data, callback) => {
     callback(true);
     socket.username = data;
     users.push(socket.username);
-  });
-
-  socket.on("getting users", (users) => {
-    console.log(users, "<--------------what is this?");
-    return (users = users);
+    console.log("hitting here?");
+    update();
   });
 
   socket.on("disconnect", (disconnect) => {
@@ -30,6 +27,10 @@ io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("new message", { data: msg, user: socket.username });
   });
+
+  function update() {
+    socket.emit("getting users", users);
+  }
 }); // I listen on the connection event for incoming sockets and log it to the console.
 
 http.listen(PORT, "0.0.0.0", () => {
